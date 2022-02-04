2b0a19af3d70f7c0d01760efb0aefe1ab85cee6d
ITstep <itstep@itstep.loc>
2022-02-04 21:48:12 +0600

    add migrations

---
+++ blog/database/migrations/2022_02_04_144209_create_titles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\DocBlock\Tags\Reference\Reference;
+
+class CreateTitlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('titles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('status', ['Announced', 'Completed', 'Ongoing', 'Suspended'])->default('Announced');
+            $table->unsignedTinyInteger('release_year')->default(DB::raw('select extract(year from current_date)'));
+            $table->text('description');
+            $table->foreignId('author_id')->constrained()->onDelete('cascade');
+            $table->foreignId('artist_id')->constrained()->onDelete('cascade');
+            $table->foreignId('publisher_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('titles');
+    }
+}
---
+++ blog/database/migrations/2022_02_04_144309_create_chapters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChaptersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('num');
+            $table->string('name');
+            $table->foreignId('title_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
