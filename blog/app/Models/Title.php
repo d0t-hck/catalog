668bb055a052a97789a9571d2e42cd9d1bc7ade7
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-02-07 17:26:24 +0600

    created models

---
+++ blog/app/Models/Title.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Title extends Model
+{
+    protected $fillable = [
+        'name',
+        'status',
+        'release_year',
+        'description',
+        'author_id',
+        'artist_id',
+        'publisher_id'
+    ];
+
+    public function genres(){
+        return $this->belongsToMany(Genre::class, 'title_genres');
+    }
+}
