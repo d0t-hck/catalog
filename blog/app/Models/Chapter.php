8713c6d4282146bb9b6b83735622f44a41394a31
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-02-15 21:28:22 +0600

    updating state action
    added rename/move folder on update.
    changed created state action to use FileHandler

--- blog/app/Models/Chapter.php
+++ blog/app/Models/Chapter.php
@@ -21,12 +21,24 @@ class Chapter extends Model
     }
 
     protected static function boot() {
+        $path = base_path(DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR);
         parent::boot();
-        self::created(function ($chapter) {
+        self::created(function ($chapter) use($path) {
             $title = Title::find($chapter->title_id, 'normalized_name');
-            $path = base_path("/public/images/{$title->normalized_name}/{$chapter->num}");
-            if (!file_exists($path)) {
-                mkdir($path);
+            return FileHandler::createFolder($path.$title->normalized_nam.DIRECTORY_SEPARATOR.$chapter->num);
+        });
+
+        self::updating(function($chapter) use($path) {
+            if ($chapter->num != $chapter->getOriginal('num')){
+                $title = Title::find($chapter->getOriginal('title_id'), 'normalized_name');
+                FileHandler::changeName($chapter->getOriginal('num'), $chapter->num, $path.$title->normalized_name.DIRECTORY_SEPARATOR);
+            }
+            if ($chapter->title_id != $chapter->getOriginal('title_id')){
+                $oldTitle = Title::find($chapter->getOriginal('title_id'), 'normalized_name');
+                $newTitle = Title::find($chapter->title_id, 'normalized_name');
+                $from = $path.$oldTitle->normalized_name.DIRECTORY_SEPARATOR.$chapter->num;
+                $to = $path.$newTitle->normalized_name.DIRECTORY_SEPARATOR.$chapter->num;
+                FileHandler::moveFiles($from, $to);
             }
         });
 
