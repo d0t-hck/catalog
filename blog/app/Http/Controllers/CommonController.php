c26bee88e80b71e803a950047c7e63321ac924c8
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-02-27 23:29:01 +0600

    move methods to common controller

--- blog/app/Http/Controllers/CommonController.php
+++ blog/app/Http/Controllers/CommonController.php
@@ -11,4 +11,26 @@ abstract class CommonController extends Controller
     public function list() {
         return response()->json(($this->getModel())::all(), 200);
     }
+
+    public function item($id) {
+        return response()->json(($this->getModel())::find($id));
+    }
+
+    public function create(Request $request) {
+        $data = $this->validate($request, $this->getModel()::getValidationRules());
+        $entity = $this->getModel()::create($data);
+        return response()->json($entity, 201);
+    }
+
+    public function update($id, Request $request) {
+        $entity = $this->getModel()::findOrFail($id);
+        $data = $this->validate($request, $this->getModel()::getValidationRules(false));
+        $entity->update($data);
+        return response()->json($entity, 200);
+    }
+
+    public function delete($id) {
+        $this->getModel()::findOrFail($id)->delete();
+        return response(204);
+    }
 }
--- blog/app/Http/Controllers/StatusController.php
+++ blog/app/Http/Controllers/StatusController.php
@@ -3,43 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Status;
-use Illuminate\Http\Request;
 
-class StatusController extends Controller
+class StatusController extends CommonController
 {
-    public function list() {
-        return response()->json(Status::all());
+    protected function getModel() {
+        return Status::class;
     }
-
-    public function item($id) {
-        return response()->json(Status::find($id));
-    }
-
-    public function create(Request $request) {
-        $this->validate($request, [
-            'code' => 'required|numeric|unique:statuses',
-            'name' => 'required|string|unique:statuses'
-        ]);
-        $status = Status::create($request->all());
-        return response()->json($status, 201);
-    }
-
-    public function update($id, Request $request) {
-        $status = Status::findOrFail($id);
-        $status->update($request->all());
-        return response()->json($status, 200);
-    }
-
-    public function delete($id) {
-        Status::findOrFail($id)->delete();
-        return response('Deleted Successfully', 200);
-    }
-
 }
-
-// class StatusController extends CommonController
-// {
-//     protected function getModel() {
-//         return Status::class;
-//     }
-// }
