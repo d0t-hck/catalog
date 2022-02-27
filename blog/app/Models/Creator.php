c2baf4e14db328640683f7e78515584d1717c059
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-02-27 23:29:21 +0600

    add validation rules

--- blog/app/Models/Creator.php
+++ blog/app/Models/Creator.php
@@ -4,13 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Creator extends Model
 {
     use HasFactory;
 
+    protected static $types = ['Author', 'Artist', 'Publisher'];
+
     protected $fillable = [
         'name', 'info', 'type'
     ];
     
+    public function getValidationRules($create = true) {
+        return $create ? [
+            'name' => 'required|string|unique:creators',
+            'info' => 'nullable',
+            'type' => [
+                'required',
+                'string',
+                Rule::in(self::$types)
+            ]
+        ] : [
+            'name' => 'string|unique:creators',
+            'info' => 'nullable',
+            'type' => [
+                'string',
+                Rule::in(self::$types)
+            ]
+            ];
+    }
 }
