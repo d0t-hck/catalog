59d4e5e9af63334990bafb50d62620f99b6ad79b
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-03-01 07:10:38 +0600

    add query params

--- blog/app/Http/Controllers/CommonController.php
+++ blog/app/Http/Controllers/CommonController.php
@@ -3,13 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 abstract class CommonController extends Controller
 {
+    private const PAGE = 10;
     abstract protected function getModel();
 
-    public function list() {
-        return response()->json(($this->getModel())::all(), 200);
+    public function list(Request $request) {
+        if ($request->has('type')){
+            $result = ($this->getModel())::where('type','ilike', '%'.$request->input('type').'%')
+                ->paginate(self::PAGE);
+            return response()->json($result);
+        }
+        if ($request->has('status') && $request->has('genre')){
+            $result = ($this->getModel())::whereHas('status', function($q) use ($request) {
+                $q->where('name','ilike', '%'.$request->input('status').'%');
+                })
+                ->whereHas('genres', function($q) use ($request) {
+                    $q->where('genre','ilike', '%'.$request->input('genre').'%');
+                })
+                ->paginate(self::PAGE);
+            return response()->json($result);
+        }
+        if ($request->has('status')){
+            $result = ($this->getModel())::whereHas('status', function(Builder $q) use ($request) {
+                    $q->where('name','ilike', '%'.$request->input('status').'%');
+                })
+                ->paginate(self::PAGE);
+            // $result = ($this->getModel())::whereRelation('statuses', 'name','ilike', '%'.$request->input('status').'%')->paginate(self::PAGE);
+            return response()->json($result);
+        }
+        if ($request->has('genre')){
+            $result = ($this->getModel())::whereHas('genres', function($q) use ($request) {
+                $q->where('genre','ilike', '%'.$request->input('genre').'%');
+            })->paginate(self::PAGE);
+            return response()->json($result);
+        }
+        return response()->json(($this->getModel())::paginate(self::PAGE));
     }
 
     public function item($id) {
