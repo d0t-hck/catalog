de9b73dd36d69b2137501a697c0ec2a317d1300f
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-03-08 22:38:24 +0600

    refactor to use 'CommonController'

--- blog/app/Http/Controllers/TitleController.php
+++ blog/app/Http/Controllers/TitleController.php
@@ -5,14 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Title;
 use Illuminate\Http\Request;
 
-class TitleController extends Controller
+class TitleController extends CommonController
 {
-    public function list() {
-        return response()->json(Title::with('genres')->paginate(10));
-    }
-
-    public function item($id) {
-        return response()->json(Title::with('genres', 'chapters')->find($id));
+    public function getModel(){
+        return Title::class;
     }
 
     public function create(Request $request) {
@@ -20,17 +16,6 @@ class TitleController extends Controller
         $title = Title::create($data);
         $title->genres()->attach($data['genres']);
         return response()->json($title, 201);
-    }
-
-    public function update($id, Request $request) {
-        $title = Title::findOrFail($id);
-        $title->update($request->all());
-        return response()->json($title, 200);
-    }
-
-    public function delete($id) {
-        Title::findOrFail($id)->delete();
-        return response('status',204);
     }
 
 }
